e6c56779d8e4ef022632b8c09e11a809c5cbd18e
Tristis <polimerazaiii@gmail.com>
2026-05-09 16:49:50 +0200

    export the subjects public API

--- src/dbmaria_utils/__init__.py
+++ src/dbmaria_utils/__init__.py
@@ -1,6 +1,6 @@
 """Python tooling for dbmaria_project."""
 
-from dbmaria_utils import projects
+from dbmaria_utils import projects, subjects
 from dbmaria_utils.connection import (
     close_pool,
     execute,
@@ -15,5 +15,6 @@ __all__ = [
     "get_connection",
     "init_pool",
     "projects",
+    "subjects",
     "transaction",
 ]
